82bf6f83dc45fb6e15a237e5b2c91ab5b5e2a453
Shanzay Khalid <shanzaykhalidfarooq@gmail.com>
2026-08-07 12:49:48 +0500

    Add set intersection practice code

---
+++ set_operations.py
@@ -0,0 +1,7 @@
+#Create two sets and find their intersection
+set_1 = {1,2,3,4,5}
+set_2 = {4,5,6,7,8}
+intersection = set_1.intersection(set_2)
+print('Set 1:', set_1)
+print('Set 2:', set_2)
+print('The intersection of the two sets is:', intersection)
