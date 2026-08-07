260524fcdb913046bff322c14b6dd42f80d6cf36
Shanzay Khalid <shanzaykhalidfarooq@gmail.com>
2026-08-07 12:54:27 +0500

    Add dictionary practice code

---
+++ dictionary_keys.py
@@ -0,0 +1,8 @@
+#Create a dictionary of your own details and print all keys
+my_details = {
+    'name' : 'Shanzay',
+    'age' : 17,
+    'city' : 'Karachi'
+}
+print('My details are:', my_details)
+print('The keys of the dictionary are:', my_details.keys())
